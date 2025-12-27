55e5aec9267760a09997ff7d2bf957ba045297b4
YZX-Clara <zixian.yang@outlook.com>
2025-12-27 17:35:21 +0800

    1

---
+++ src/main/java/com/example/web/dto/InheritorDto.java
@@ -0,0 +1,98 @@
+package com.example.web.dto;
+import com.example.web.enums.*;
+import com.example.web.tools.dto.BaseDto;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.sql.Date;
+import java.sql.Timestamp;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.example.web.entity.*;
+import org.apache.commons.beanutils.BeanUtils;
+import java.lang.reflect.InvocationTargetException;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+/**
+ * 传人类
+ */
+@Data
+public class InheritorDto extends BaseDto
+{
+
+
+
+    /**
+     * 姓名
+     */
+    @JsonProperty("Name")
+    private String Name;
+
+
+    /**
+     * 头像
+     */
+    @JsonProperty("ImageUrls")
+    private String ImageUrls;
+
+
+    /**
+     * 内容
+     */
+    @JsonProperty("Content")
+    private String Content;
+
+
+    /**
+     * 性别
+     */
+    @JsonProperty("Sex")
+    private String Sex;
+
+
+    /**
+     * 民族
+     */
+    @JsonProperty("Nation")
+    private String Nation;
+
+
+    /**
+     * 编号
+     */
+    @JsonProperty("No")
+    private String No;
+
+
+    /**
+     * 出生年月
+     */
+    @JsonSerialize(using= LocalDateTimeSerializer.class)
+    @JsonDeserialize(using= LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonProperty("BirthDate")
+    private LocalDateTime BirthDate;
+
+
+    /**
+     * 省市区
+     */
+    @JsonProperty("ProviceCityArea")
+    private String ProviceCityArea;
+
+    /**
+     * 把传人传输模型转换成传人实体
+     */
+    public Inheritor MapToEntity() throws InvocationTargetException, IllegalAccessException {
+        Inheritor Inheritor= new Inheritor();
+
+        BeanUtils.copyProperties(Inheritor,this);
+
+        return Inheritor;
+    }
+
+}
---
+++ src/main/java/com/example/web/service/InheritorService.java
@@ -0,0 +1,41 @@
+package com.example.web.service;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.web.dto.*;
+import com.example.web.dto.query.*;
+import com.example.web.entity.*;
+import com.example.web.tools.dto.*;
+import com.example.web.enums.*;
+import java.lang.reflect.InvocationTargetException;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.io.IOException;
+import jakarta.servlet.http.HttpServletResponse;
+/**
+ * 传人功能的Service接口的定义清单
+ */
+public interface InheritorService extends IService<Inheritor> {
+
+    /**
+     * 传人的分页查询方法接口定义
+     */
+    public PagedResult<InheritorDto> List(InheritorPagedInput input) ;
+    /**
+     * 传人的新增或者修改方法接口定义
+     */
+    public InheritorDto CreateOrEdit(InheritorDto input);
+
+    /**
+     * 获取传人信息
+     */
+    public InheritorDto Get(InheritorPagedInput input);
+    /**
+     * 传人删除
+     */
+    public void Delete(IdInput input);
+
+    /**
+     * 传人批量删除
+     */
+    public void BatchDelete(IdsInput input);
+
+
+}
